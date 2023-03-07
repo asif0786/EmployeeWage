116be4d236f864c9b6ddc63761b906d0c0100e26
ASIF <imdasif9@gmail.com>
2023-03-07 11:11:06 +0530

    Calculating Wages for a Month

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,19 @@ public class Main {
         int FDHour = 8;
         int PTHour = 4;
         int WPHour = 20;
+        int TWDay = 20;
 
         switch (random){
             case 0:
                 System.out.println("Employee is Absent");
                 break;
             case 1:
-                int  DEWage = FDHour * WPHour;
-                System.out.println("Daily Full Time Employee Wage = Rs."+DEWage);
+                int  DEWage = FDHour * WPHour * TWDay;
+                System.out.println("Monthly Full Time Employee Wage = Rs."+DEWage);
                 break;
             case 2:
-                int PTWage = PTHour * WPHour;
-                System.out.println("Daily Part Time Employee Wage = Rs."+PTWage);
+                int PTWage = PTHour * WPHour * TWDay;
+                System.out.println("Monthly Part Time Employee Wage = Rs."+PTWage);
                 break;
         }
 
