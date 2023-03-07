389a07a1452b93c1a1112526d0889942cfc627d1
ASIF <imdasif9@gmail.com>
2023-03-07 11:01:23 +0530

    Added Part Time

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,17 @@ public class Main {
     public static void main(String[] args) {
 
         // Generate random number
-        int random = (int)(Math.random() * 2) + 0;
+        int random = (int)(Math.random() * 3) + 0;
         int FDHour = 8;
+        int PTHour = 4;
         int WPHour = 20;
         if(random == 1){
             int  DEWage = FDHour * WPHour;
-            System.out.println("Daily Employee Wage = Rs."+DEWage);
-        }else {
+            System.out.println("Daily Full Time Employee Wage = Rs."+DEWage);
+        } else if (random == 2) {
+            int PTWage = PTHour * WPHour;
+            System.out.println("Daily Part Time Employee Wage = Rs."+PTWage);
+        } else {
             System.out.println("Employee is Absent");
         }
 
